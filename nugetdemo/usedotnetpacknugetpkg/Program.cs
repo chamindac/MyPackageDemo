ad5812ef871e7448c34ca0069d2d25c8d4c29682
Chaiminda Chandrasekara <chaminda@ekriegers.com>
2020-12-13 01:31:08 +0530

    Use nuget package in github

---
+++ nugetdemo/usedotnetpacknugetpkg/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using mydotnetpacknugetpkg;
+
+namespace usedotnetpacknugetpkg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(new DemoPackageDotnetPack().HelloWorldNugetDemo());
+            Console.ReadLine();
+        }
+    }
+}
