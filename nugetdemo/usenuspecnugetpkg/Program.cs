317d99bf613f9e360e9ae2fa3696932342d5556d
Chaiminda Chandrasekara <chaminda@ekriegers.com>
2020-12-13 10:55:29 +0530

    nuget packge with nuspec usage sample

---
+++ nugetdemo/usenuspecnugetpkg/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using mynuspecnugetpkg;
+
+namespace usenuspecnugetpkg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(new DemoPackageNuspec().HelloWorldNugetDemo());
+            Console.ReadLine();
+        }
+    }
+}
