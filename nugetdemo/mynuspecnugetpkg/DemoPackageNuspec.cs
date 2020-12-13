5ccf4d291a68dabd89b12e23146e445bae4527ac
Chaiminda Chandrasekara <chaminda@ekriegers.com>
2020-12-13 09:32:15 +0530

    add nuspec nugetpkg project

---
+++ nugetdemo/mynuspecnugetpkg/DemoPackageNuspec.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mynuspecnugetpkg
+{
+    public class DemoPackageNuspec
+    {
+        public string HelloWorldNugetDemo()
+        {
+            return "Hello world! Welcome to nuget packages with nuspec!";
+        }
+    }
+}
